5b056b6690613794b66a10a0ff8f48a50eafb0bd
s2selyn <s2selyn@gmail.com>
2025-08-21 11:44:00 +0900

    end to git

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -12,6 +12,20 @@ public class TestClass {
 		
 	}
 	
+	public void pull() {
+		System.out.println("Pull을 잘하자");
+	}
+	
+	public void printStatement() {
+		
+		System.out.println("1. 워크스페이스에서 gitBash를 실행한다.");
+		System.out.println("2. git pull origin master");
+		System.out.println("3. git add .");
+		System.out.println("4. git commit -m 뭐했는지");
+		System.out.println("5. git push origin master");
+		
+	}
+	
 	public void hi() {
 		System.out.println("가보자구~~~~~");
 	}
