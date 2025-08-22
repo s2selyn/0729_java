3b9a10b4816f5016e05891a34806f6c0e9dc3a92
s2selyn <s2selyn@gmail.com>
2025-08-22 12:28:44 +0900

    setup mainMenu

--- CollectionPractice/src/com/kh/practice/list/music/view/MusicView.java
+++ CollectionPractice/src/com/kh/practice/list/music/view/MusicView.java
@@ -6,12 +6,59 @@ import com.kh.practice.list.music.controller.MusicController;
 
 public class MusicView {
 	
+	// 스캐너 객체 생성
 	private Scanner sc = new Scanner(System.in);
+	
+	// MusicController 객체 생성
 	private MusicController mc = new MusicController();
 	
 	// 사용자가 직접 메인 메뉴를 선택 할 수 있음. 종료 전까지 반복 실행. 각 메뉴를 누를 시 해당 메소드로 이동.
 	public void mainMenu() {
 		
+		/*
+		******* 메인 메뉴 ******* 
+		1. 마지막 위치에 곡 추가  // addList() 실행
+		2. 첫 위치에 곡 추가  // addAtZero()
+		3. 전체 곡 목록 출력  // printAll()
+		4. 특정 곡 검색   // searchMusic()
+		5. 특정 곡 삭제   // removeMusic()
+		6. 특정 곡 정보 수정  // setMusic()
+		7. 곡명 오름차순 정렬  // ascTitle()
+		8. 가수명 내림차순 정렬  // descSinger()
+		9. 종료    // “종료” 출력 후 main()으로 리턴
+		메뉴 번호 선택 : >> 입력 받음 
+		// 메뉴 화면 반복 실행 처리
+		*/
+		
+		while(true) {
+			
+			System.out.println("******* 메인 메뉴 *******");
+			System.out.println("1. 마지막 위치에 곡 추가");
+			System.out.println("2. 첫 위치에 곡 추가");
+			System.out.println("3. 전체 곡 목록 출력");
+			System.out.println("4. 특정 곡 검색");
+			System.out.println("5. 특정 곡 삭제");
+			System.out.println("6. 특정 곡 정보 수정");
+			System.out.println("7. 곡명 오름차순 정렬");
+			System.out.println("8. 가수명 내림차순 정렬");
+			System.out.println("9. 종료");
+			System.out.print("메뉴 번호 선택 : >> ");
+			int menuNo = sc.nextInt();
+			sc.nextLine();
+			
+			switch(menuNo) {
+			case 1 : addList(); break;
+			case 2 : addAtZero(); break;
+			case 3 : printAll(); break;
+			case 4 : searchMusic(); break;
+			case 5 : removeMusic(); break;
+			case 6 : setMusic(); break;
+			case 7 : ascTitle(); break;
+			case 8 : descSinger(); break;
+			case 9 : System.out.println("종료"); return;
+			}
+			
+		}
 		
 	}
 	
