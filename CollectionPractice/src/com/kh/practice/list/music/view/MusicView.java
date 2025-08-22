d6d07f00c855cab1f7b74524d672748db0ff0c90
s2selyn <s2selyn@gmail.com>
2025-08-22 12:21:26 +0900

    setup MusicView

--- CollectionPractice/src/com/kh/practice/list/music/view/MusicView.java
+++ CollectionPractice/src/com/kh/practice/list/music/view/MusicView.java
@@ -1,58 +1,65 @@
 package com.kh.practice.list.music.view;
 
+import java.util.Scanner;
+
+import com.kh.practice.list.music.controller.MusicController;
+
 public class MusicView {
 	
+	private Scanner sc = new Scanner(System.in);
+	private MusicController mc = new MusicController();
+	
+	// 사용자가 직접 메인 메뉴를 선택 할 수 있음. 종료 전까지 반복 실행. 각 메뉴를 누를 시 해당 메소드로 이동.
 	public void mainMenu() {
 		
-		// 사용자가 직접 메인 메뉴를 선택 할 수 있음. 종료 전까지 반복 실행. 각 메뉴를 누를 시 해당 메소드로 이동.
 		
 	}
 	
+	// 마지막 위치에 곡 추가 성공을 알리는 메소드
 	public void addList() {
 		
-		// 마지막 위치에 곡 추가 성공을 알리는 메소드
 		
 	}
 	
+	// 첫 위치에 곡 추가 성공을 알리는 하는 메소드
 	public void addAtZero() {
 		
-		// 첫 위치에 곡 추가 성공을 알리는 하는 메소드
 		
 	}
 	
+	// 전체 곡 목록을 출력 성공을 알리는 메소드
 	public void printAll() {
 		
-		// 전체 곡 목록을 출력 성공을 알리는 메소드
 		
 	}
 	
+	// 특정 곡 검색의 결과를 보여주는 메소드
 	public void searchMusic() {
 		
-		// 특정 곡 검색의 결과를 보여주는 메소드
 		
 	}
 	
+	// 특정 곡 삭제 결과를 보여주는 메소드
 	public void removeMusic() {
 		
-		// 특정 곡 삭제 결과를 보여주는 메소드
 		
 	}
 	
+	// 특정 곡 정보 수정 결과를 보여주는 메소드
 	public void setMusic() {
 		
-		// 특정 곡 정보 수정 결과를 보여주는 메소드
 		
 	}
 	
+	// 곡 명 오름차순 정렬 성공을 알리는 메소드
 	public void ascTitle() {
 		
-		// 곡 명 오름차순 정렬 성공을 알리는 메소드
 		
 	}
 	
+	// 가수 명 내림차순 정렬 성공을 알리는 메소드
 	public void descSinger() {
 		
-		// 가수 명 내림차순 정렬 성공을 알리는 메소드
 		
 	}
 
