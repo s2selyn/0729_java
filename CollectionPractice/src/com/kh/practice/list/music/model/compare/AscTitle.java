8d0665e9f388fff16ca6c1e69ead31495b08acb9
s2selyn <s2selyn@gmail.com>
2025-08-22 12:22:00 +0900

    setup AscTitle

--- CollectionPractice/src/com/kh/practice/list/music/model/compare/AscTitle.java
+++ CollectionPractice/src/com/kh/practice/list/music/model/compare/AscTitle.java
@@ -2,9 +2,9 @@ package com.kh.practice.list.music.model.compare;
 
 public class AscTitle {
 	
+	// 곡 명을 오름 차순 정렬의 기준을 제공하는 메소드
 	public int compare(Object o1, Object o2) {
 		
-		// 곡 명을 오름 차순 정렬의 기준을 제공하는 메소드
 		return 0;
 		
 	}
