baffbb2719508d623414701995a6ea61cfaaa0c0
s2selyn <s2selyn@gmail.com>
2025-08-22 11:53:38 +0900

    edit Book Class

--- CollectionPractice/src/com/kh/practice/list/library/model/vo/Book.java
+++ CollectionPractice/src/com/kh/practice/list/library/model/vo/Book.java
@@ -7,44 +7,73 @@ public class Book {
 	private String category;
 	private int price;
 	
+	// 기본 생성자
 	public Book() {
 		
-		// 기본 생성자
-		
 	}
 	
+	// 매개변수가 있는 생성자
 	public Book(String title, String author, String category, int price) {
 		
-		// 매개변수가 있는 생성자
-		
 	}
 	
-	// 
+	// setter() / getter()
+	public void setTitle(String title) {
+		this.title = title;
+	}
 	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	// 객체의 정보를 리턴하는 메소드
 	public String toString() {
 		
-		// 객체의 정보를 리턴하는 메소드
 		return null;
 		
 	}
 	
+	// 해시코드를 리턴하는 메소드
 	public int hashCode() {
 		
-		// 해시코드를 리턴하는 메소드
 		return 0;
 		
 	}
 	
+	// 객체가 같은지 여부를 리턴하는 메소드
 	public boolean equals() {
 		
-		// 객체가 같은지 여부를 리턴하는 메소드
 		return false;
 		
 	}
 	
+	// 정렬 기준을 가지고 있는 메소드
 	public int compareTo(Object o) {
 		
-		// 정렬 기준을 가지고 있는 메소드
 		return 0;
 		
 	}
